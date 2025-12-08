3b341539767e170224e9565d796a5f67ef0d2a3a
Lucas Fernando <ferlucas074@gmail.com>
2025-12-07 22:24:13 -0300

    criação de rota post e get retornando usuario com sucesso

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios' , (req, res) => {
+
+  users.push(req.body)
+
+  res.send('Usuário criado com sucesso')
+  
+})
+
+
 
 app.get('/usuarios',  (req, res) => {
-  res.send('Lista de usuarios')
+  res.json(users)
+
 })
 
 app.listen(3000)
+
+/*
+  Lista de objetivos a serem alcançados:
+
+  - Criar um usuário
+  - Listar todos os usuários
+  - Editar um usuário
+  - Deletar um usuário
+*/
+
